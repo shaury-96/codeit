bcb0590c761d429c5a548131dcfcacd841a3fcda
Shaury Saxena <shaurydeepsaxena@gmail.com>
2023-08-02 00:06:35 +0530

    userlogin_done

--- codeit/__init__.py
+++ codeit/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '60ba11abf2de9ed6d6495f367b3d65d2'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codeit'
 db = SQLAlchemy(app)
+bcrypt=Bcrypt(app)
+login_manager=LoginManager(app)
+login_manager.login_view='login'
+login_manager.login_message_category='info'
 
 from codeit import routes
