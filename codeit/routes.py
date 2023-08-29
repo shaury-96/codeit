3fc8dabdb804dba2cd09c20f38066a0626a1c447
Shaury Saxena <shaurydeepsaxena@gmail.com>
2023-08-29 16:05:07 +0530

    color scheme changed final

--- codeit/routes.py
+++ codeit/routes.py
@@ -13,7 +13,7 @@ from datetime import datetime
 @app.route("/home")
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=1)
     return render_template('home.html', posts=posts)
 
 
