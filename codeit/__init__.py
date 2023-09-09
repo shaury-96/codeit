2870459c9782c131802c6c46394cff229d4b48b0
Shaury Saxena <shaurydeepsaxena@gmail.com>
2023-09-09 23:19:42 +0530

    files updated

--- codeit/__init__.py
+++ codeit/__init__.py
@@ -1,8 +1,13 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_moment import Moment
+from flask_mail import Mail
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '60ba11abf2de9ed6d6495f367b3d65d2'
@@ -13,5 +18,14 @@ bcrypt=Bcrypt(app)
 login_manager=LoginManager(app)
 login_manager.login_view='login'
 login_manager.login_message_category='info'
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+# app.config['MAIL_USERNAME'] ='codeit.step@gmail.com'
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+# app.config['MAIL_PASSWORD'] = 'tjazfuizekmuhqqj'
+mail = Mail(app)
 
 from codeit import routes
