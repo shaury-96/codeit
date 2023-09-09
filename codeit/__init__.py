70f6483f64c298ff94cb3862f02474b16688f5f5
Shaury Saxena <shaurydeepsaxena@gmail.com>
2023-09-10 02:43:03 +0530

    updated env

--- codeit/__init__.py
+++ codeit/__init__.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '60ba11abf2de9ed6d6495f367b3d65d2'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codeit'
 moment=Moment(app)
 db = SQLAlchemy(app)
@@ -23,9 +23,7 @@ app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-# app.config['MAIL_USERNAME'] ='codeit.step@gmail.com'
 app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-# app.config['MAIL_PASSWORD'] = 'tjazfuizekmuhqqj'
 mail = Mail(app)
 
 from codeit import routes
